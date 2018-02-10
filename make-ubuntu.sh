6e8eccf0e02e236701c48c5c5ed3f7a21626387c
Adam Novak <interfect@gmail.com>
2018-02-09 22:31:26 -0800

    Set to build the 20.1 tag and add my own binaries

--- make-ubuntu.sh
+++ make-ubuntu.sh
@@ -5,8 +5,10 @@
 sudo apt install nsis nsis-pluginapi mingw-w64 -y
 
 if [ ! -z "$REBUILD_CJDNS" ]; then
+  # Set REBUILD_CJDNS=1 to rebuild
   ([ ! -e cjdns ] && git clone git@github.com:cjdelisle/cjdns) || git -C cjdns pull # clone to cjdns or pull if already cloned
   cd cjdns
+  git checkout cjdns-v20.1
   SYSTEM=win32 CROSS_COMPILE=i686-w64-mingw32- ./cross-do
   cp *.exe ../installation
   cd ..
