dc2e8e1ef82eb66bd6a5c8620a44269da78e6bbe
Andras Fuchs <andras.fuchs@gmail.com>
2022-07-13 11:15:35 +0200

    Add the execution step for fixBrokenConfig.vbs

--- installation/genconf.cmd
+++ installation/genconf.cmd
@@ -2,6 +2,10 @@ if exist cjdroute.conf (
 	rem Not clobbering config
 ) else (
 	cjdroute.exe --genconf > cjdroute.conf
+	if exist fixBrokenConfig.vbs (
+		rem Fix the problem with the generated config file
+		cscript fixBrokenConfig.vbs
+	)
 	if exist addPublicPeers.vbs (
 		rem Add public peers to config file
 		cscript addPublicPeers.vbs
